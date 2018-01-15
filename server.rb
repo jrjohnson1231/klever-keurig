d509a738b98b01492468d36efb9d2a44b38cf67a
John Johnson <jrjohnson1231@gmail.com>
2018-01-15 22:24:09 +0000

    basic socket

--- server.rb
+++ server.rb
@@ -1,7 +1,23 @@
 # myapp.rb
 require 'sinatra'
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :sockets, []
 
 get '/' do
-  puts 'Hello world!'
-  'Hello world!'
+  if !request.websocket?
+    EM.next_tick { settings.sockets.each{|s| s.send('Got a message') } }
+    return 'Hello, world!'
+  else
+    request.websocket do |ws|
+      ws.onopen do
+	ws.send('Hello!')
+        settings.sockets << ws
+      end
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
